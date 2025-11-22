a1e243282a1266fe9ce690090653c8eb077fc9b1
Jose-11235 <jaab12358@gmail.com>
2025-11-22 02:35:13 -0500

    Shield Prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     public int lives;
     private float speed;
+    private int weaponType;
 
     private GameManager gameManager;
 
@@ -15,6 +16,8 @@ public class PlayerController : MonoBehaviour
 
     public GameObject bulletPrefab;
     public GameObject explosionPrefab;
+    public GameObject thrusterPrefab;
+    public GameObject shieldPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -34,22 +37,128 @@ public class PlayerController : MonoBehaviour
 
     public void LoseALife()
     {
-        //lives = lives - 1;
-        //lives -= 1;
-        lives--;
-        gameManager.ChangeLivesText(lives);
+        //Do I have a shield? If yes: do not lose a life, but instead deactivate the shield's visibility
+        //If not: lose a life
+        if (shieldPrefab != null && shieldPrefab.activeInHierarchy)
+        {
+            shieldPrefab.SetActive(false);
+        } 
+        else
+        {
+            lives--;
+            gameManager.ChangeLivesText(lives);
+        }
+        
         if (lives == 0)
         {
             Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            gameManager.GameOver();
             Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator SpeedPowerDown()
+    {
+        yield return new WaitForSeconds(3f);
+        speed = 5f;
+        thrusterPrefab.SetActive(false);
+        gameManager.ManagePowerupText(0);
+        gameManager.PlaySound(2);
+    }
+
+    IEnumerator WeaponPowerDown()
+    {
+        yield return new WaitForSeconds(3f);
+        weaponType = 1;
+        gameManager.ManagePowerupText(0);
+        gameManager.PlaySound(2);
+    }
+
+        IEnumerator ShieldPowerDown()
+    {
+        yield return new WaitForSeconds(3f);
+        shieldPrefab.SetActive(false);
+        gameManager.ManagePowerupText(0);
+        gameManager.PlaySound(2);
+    }
+
+    private void OnTriggerEnter2D(Collider2D whatDidIHit)
+    {
+        if(whatDidIHit.tag == "Powerup")
+        {
+            Destroy(whatDidIHit.gameObject);
+            int whichPowerup = Random.Range(1, 5);
+            gameManager.PlaySound(1);
+            switch (whichPowerup)
+            {
+                case 1:
+                    //Picked up speed
+                    speed = 10f;
+                    StartCoroutine(SpeedPowerDown());
+                    thrusterPrefab.SetActive(true);
+                    gameManager.ManagePowerupText(1);
+                    break;
+                case 2:
+                    weaponType = 2; //Picked up double weapon
+                    StartCoroutine(WeaponPowerDown());
+                    gameManager.ManagePowerupText(2);
+                    break;
+                case 3:
+                    weaponType = 3; //Picked up triple weapon
+                    StartCoroutine(WeaponPowerDown());
+                    gameManager.ManagePowerupText(3);
+                    break;
+                case 4:
+                    //Picked up shield
+                    if (shieldPrefab != null && shieldPrefab.activeInHierarchy)
+                    {
+                        break;
+                    } 
+                    else
+                    {
+                        StartCoroutine(ShieldPowerDown());
+                        shieldPrefab.SetActive(true);
+                        gameManager.ManagePowerupText(4);
+                        break;
+                    }
+
+            }
+        }
+        else if(whatDidIHit.tag == "Health")
+        {
+            Destroy(whatDidIHit.gameObject);
+            if(lives < 3)
+            {
+                lives ++;
+                gameManager.ChangeLivesText(lives);
+            }
+            else
+            {
+                gameManager.AddScore(1);
+            }
+            gameManager.PlaySound(1);
         }
     }
 
     void Shooting()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(bulletPrefab, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+            switch(weaponType)
+            {
+                case 1:
+                    Instantiate(bulletPrefab, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(bulletPrefab, transform.position + new Vector3(-0.5f, 0.5f, 0), Quaternion.identity);
+                    Instantiate(bulletPrefab, transform.position + new Vector3(0.5f, 0.5f, 0), Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(bulletPrefab, transform.position + new Vector3(-0.5f, 0.5f, 0), Quaternion.Euler(0, 0, 45));
+                    Instantiate(bulletPrefab, transform.position + new Vector3(0, 0.5f, 0), Quaternion.identity);
+                    Instantiate(bulletPrefab, transform.position + new Vector3(0.5f, 0.5f, 0), Quaternion.Euler(0, 0, -45));
+                    break;
+            }
         }
     }
 
@@ -62,35 +171,14 @@ public class PlayerController : MonoBehaviour
         float horizontalScreenSize = gameManager.horizontalScreenSize;
         float verticalScreenSize = gameManager.verticalScreenSize;
 
-        if (transform.position.x <= -horizontalScreenSize)
+        if (transform.position.x <= -horizontalScreenSize || transform.position.x > horizontalScreenSize)
         {
-            transform.position = new Vector3(horizontalScreenSize, transform.position.y, transform.position.z);
-        }
-        else if (transform.position.x > horizontalScreenSize)
-        {
-            transform.position = new Vector3(-horizontalScreenSize, transform.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
         }
 
-        // Player boundary
-        if (transform.position.y > 0f)
+        if (transform.position.y <= -verticalScreenSize || transform.position.y > verticalScreenSize)
         {
-            transform.position = new Vector3(transform.position.x, 0f, transform.position.z);
-        }
-        else if (transform.position.y < -4f)
-        {
-            transform.position = new Vector3(transform.position.x, -4f, transform.position.z);
-        }
-    }
-        public void GetALife()
-        {
-        if (lives < 3)
-        {
-            lives++;
-            gameManager.ChangeLivesText(lives);
-        }
-        else
-        {
-            gameManager.AddScore(1);
+            transform.position = new Vector3(transform.position.x, transform.position.y * -1, 0);
         }
 
     }
